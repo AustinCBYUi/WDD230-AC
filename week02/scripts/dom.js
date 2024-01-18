ff874127ea959d585070433dcfd4b603d1e66bbc
AustinCBYUi <131830841+AustinCBYUi@users.noreply.github.com>
2024-01-17 22:48:37 -0700

    Still working on this section

---
+++ week02/scripts/dom.js
@@ -0,0 +1,9 @@
+// - Constants - \\
+//Selects the for="favchap" in the input tag of the html.
+const input = document.querySelector("#favchap");
+
+//You don't use a hashtag if it isn't an id.
+const button = document.querySelector("button");
+
+//Selects the list id in the html
+const list = document.querySelector("#list");
