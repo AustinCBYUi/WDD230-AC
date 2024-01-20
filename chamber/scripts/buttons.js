6a93167f03fa3f68f52fabf80625057c59e15e01
AustinCBYUi <131830841+AustinCBYUi@users.noreply.github.com>
2024-01-19 22:55:06 -0700

    Chamber Home Page WIP, placeholder images for spotlight

---
+++ chamber/scripts/buttons.js
@@ -0,0 +1,12 @@
+
+const bodies = document.querySelector("body");
+const eventsButton = document.querySelector("#events-button");
+const becomeMemberButton = document.querySelector("#become-member");
+
+eventsButton.addEventListener("click", () => {
+    alert("This content has not been implemented yet!")    
+});
+
+becomeMemberButton.addEventListener("click", () => {
+    alert("This content has not been implemented yet!");
+});
