83f7c40f22402e9af0b781c04bd507e158ae6ddf
AustinCBYUi <131830841+AustinCBYUi@users.noreply.github.com>
2024-01-28 00:06:44 -0700

    Final commit for finish

--- chamber/scripts/visitedPage.js
+++ chamber/scripts/visitedPage.js
@@ -1,42 +1,32 @@
-// milliseconds to days constant = 1000 ms/s * 60 s/m * 60 m/h * 24 h/day
-const msToDays = 84600000;
+
 // today's date
 const theDateToday = new Date();
 
 // initialize display elements
 const visited = document.querySelector("#visit");
 
-let numOfVisits = Number(window.localStorage.getItem("visit-ac")) || 0;
-let dateOfVisit = Date(window.localStorage.getItem("visitDate-ac")) || 0;
-let dateOfLastVisit = Date(window.localStorage.getItem("lastVisit-ac")) || 0;
+//Needed to reconstruct a new date.
+let getLastVisit = new Date(localStorage.getItem("lastVisit-ac")) || 0;
 
-dateOfVisit = theDateToday;
+//Converts days into specific days of month.
+let tomorrow = theDateToday.getUTCDate() + 1; //+ 1 works to add a day
+let lastVis = getLastVisit.getUTCDate();
 
-if (numOfVisits !== 0) {
-    visited.textContent = "Last visit is " + dateOfLastVisit + " and today is " + theDateToday;
+//Subtracts last time the page was visited from today.
+let mathForLastVis = lastVis - theDateToday.getUTCDate();
+
+
+//Logic, if the difference is less than or equal to .99, then it will display Back so soon..
+if (lastVis - tomorrow <= .99) {
+    visited.textContent = "Back so soon! Awesome!";
+//If the last visit is greater than 1, it will say how many days it's been since your last visit.
+} else if (lastVis - tomorrow > 1) {
+    visited.textContent = `You last visited ${mathForLastVis}`;
+//This condition is if the user has never visited before.
 } else {
     visited.textContent = "Welcome! Let us know if you have any questions!";
 }
 
-numOfVisits++;
-dateOfLastVisit++;
+//Storage
+window.localStorage.setItem("lastVisit-ac", theDateToday);
 
-localStorage.setItem("visit-ac", numOfVisits);
-localStorage.setItem("visitDate-ac", dateOfVisit);
-localStorage.setItem("lastVisit-ac", dateOfLastVisit);
-
-
-// processing
-const today = Date.now();
-const christmasDate = new Date(Date.UTC(theDateToday.getFullYear(), 11, 25));
-// check if is the waing days of December, if so ... change to next year.
-if (theDateToday.getMonth() == 11 && theDateToday.getDate() > 25) {
-	christmasDate.setFullYear(christmasDate.getFullYear() + 1);
-}
-// find difference between epoch times in ms and convert to days
-let daysleft = (christmasDate.getTime() - Date.now()) / msToDays;
-
-todayElement.textContent = today;
-christmasElement.textContent = christmasDate.getTime();
-christmasDateElement.textContent = christmasDate;
-daysElement.textContent = `${daysleft.toFixed(0)} days`;
