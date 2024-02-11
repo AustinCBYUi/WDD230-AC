1902fbc099d72147f3043099350a8e472012818b
AussieDev <131830841+AustinCBYUi@users.noreply.github.com>
2024-02-10 19:50:18 -0700

    Commit

--- chamber/scripts/fetchdirectory.js
+++ chamber/scripts/fetchdirectory.js
@@ -1,2 +1,28 @@
 const baseURL = "https://AustinCBYUi.github.io/wdd230/";
-const dataURL = "";
+const dataURL = `${baseURL}chamber/data/members.json`;
+const populate = document.querySelector("#populate");
+
+async function getData() {
+    try {
+        const response = await fetch(dataURL);
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data);
+            displayData(data.info);
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const displayData = (infos) => {
+    infos.forEach((info) => {
+        console.log(info.member);
+        let div = document.createElement("div");
+
+    });
+}
+
+getData();
