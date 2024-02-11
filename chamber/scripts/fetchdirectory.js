9d7ac548a657e62fd08ca0664d9b3236c5e0d011
AustinCBYUi <131830841+AustinCBYUi@users.noreply.github.com>
2024-02-10 21:18:03 -0700

    Fix json again?

--- chamber/scripts/fetchdirectory.js
+++ chamber/scripts/fetchdirectory.js
@@ -17,10 +17,25 @@ async function getData() {
     }
 }
 
+var counter = -1;
 const displayData = (infos) => {
     infos.forEach((info) => {
-        console.log(info.member);
+        counter ++;
+        console.log(info); //info.members[3].name works, info.members[3] works
         let div = document.createElement("div");
+
+        let h3 = document.createElement("h3");
+        let p = document.createElement("p");
+
+        h3.innerHTML = info.members.name;
+        p.textContent = info.members.phone;
+
+        div.className = "box";
+
+        div.appendChild(h3);
+        div.appendChild(p);
+
+        populate.appendChild(div);
 
     });
 }
